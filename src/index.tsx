4a6d8f518f4f413da88fcdfd870f4af2c1f3ef35
mohammadmaleh <mohammad.maleh@gmail.com>
2022-06-25 12:46:15 +0200

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/** @format */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      test={function (): void {
+        throw new Error("Function not implemented.");
+      }}
+    />
   </React.StrictMode>
 );
 
